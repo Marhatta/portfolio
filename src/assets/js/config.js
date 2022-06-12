12e8b8c4266bcb75e957b70b9987e380139a49b6
Marhatta <vishal.marhatta@gmail.com>
2022-06-12 23:22:05 +0530

    Added contact, footer sections

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -3,3 +3,6 @@ import '../styles/loader.scss';
 import '../styles/landing.scss';
 import '../styles/fancyLinks.scss';
 import '../styles/fancyButton.scss';
+import '../styles/skills.scss';
+import '../styles/projects.scss';
+import '../styles/contact.scss';
