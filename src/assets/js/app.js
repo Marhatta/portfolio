67189a685938c47140e978e9ec6c2577926c4e36
Vishal Marhatta <vishal.marhatta@Eternus.local>
2022-07-05 13:26:58 +0530

    Completed landing screen

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,40 +1,40 @@
-import gsap from 'gsap';
-const bar = document.querySelector('.loading__bar--inner');
-const counter_num = document.querySelector('.loading__counter--number');
+import gsap from "gsap";
+const bar = document.querySelector(".loading__bar--inner");
+const counter_num = document.querySelector(".loading__counter--number");
 let counter = 0;
 let barInterval = setInterval(() => {
-  bar.style.width = counter + '%';
-  counter_num.innerText = counter + '%';
+  bar.style.width = counter + "%";
+  counter_num.innerText = counter + "%";
   counter++;
   if (counter === 101) {
     clearInterval(barInterval);
-    gsap.to('.loading__bar', {
+    gsap.to(".loading__bar", {
       duration: 5,
-      rotate: '90deg',
-      left: '1000%',
+      rotate: "90deg",
+      left: "1000%",
     });
-    gsap.to('.loading__text,.loading__counter', {
+    gsap.to(".loading__text,.loading__counter", {
       duration: 0.5,
       opacity: 0,
     });
-    gsap.to('.loading__box', {
+    gsap.to(".loading__box", {
       duration: 1,
-      height: '500px',
-      borderRadius: '50%',
+      height: "500px",
+      borderRadius: "50%",
     });
-    gsap.to('.loading__svg', {
+    gsap.to(".loading__svg", {
       opacity: 1,
       duration: 10,
-      rotate: '360deg',
+      rotate: "360deg",
     });
-    gsap.to('.loading__box', {
+    gsap.to(".loading__box", {
       delay: 2,
-      border: 'none',
+      border: "none",
     });
-    gsap.to('.loading', {
+    gsap.to(".loading", {
       delay: 2,
       duration: 0.5,
-      background: 'transparent',
+      background: "transparent",
       opacity: 0,
       zIndex: 1,
     });
--- src/assets/js/threeBg.js
+++ src/assets/js/threeBg.js
@@ -1,5 +1,8 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import shape1 from "../images/shapes/24.jpg";
 
+const container = document.querySelector(".three_bg");
+const loader = new THREE.TextureLoader();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   70,
@@ -8,20 +11,47 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const renderer = new THREE.WebGL1Renderer();
+const renderer = new THREE.WebGL1Renderer({
+  antialias: true,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+container.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// responsive
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+const geometry = new THREE.PlaneGeometry(22, 10, 10, 9);
+const material = new THREE.MeshBasicMaterial({
+  map: loader.load(shape1),
+});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 camera.position.z = 5;
+const count = geometry.attributes.position.count;
+const clock = new THREE.Clock();
 
 function animate() {
+  const time = clock.getElapsedTime();
+  for (let i = 0; i < count; i++) {
+    const x = geometry.attributes.position.getX(i);
+    const y = geometry.attributes.position.getY(i);
+
+    // animations
+    const anim1 = 0.75 * Math.sin(x * 2 + time * 0.5);
+    // const anim2 = 0.25 * Math.sin(x + time * 0.7);
+    // const anim3 = 0.75 * Math.sin(y * 15 + time * 0.7);
+
+    geometry.attributes.position.setZ(i, anim1);
+    geometry.computeVertexNormals();
+    geometry.attributes.position.needsUpdate = true;
+  }
   requestAnimationFrame(animate);
-  mesh.rotation.x += 0.1;
-  mesh.rotation.y += 0.1;
+  // mesh.rotation.x += 0.01;
+  // mesh.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 
